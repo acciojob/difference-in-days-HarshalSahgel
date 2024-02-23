90e3449ab2440c5fe93765f22ed68f343fe2bae9
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-02-23 23:00:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  const onday = 24 * 60 * 60 * 1000;
+	const firstDay = new Date(date1);
+	const secondDay = new Date(date2);
+
+	const DifferenceBtw = (secondDay.getTime() - firstDay.getTime());
+
+	const accurateDiff = Math.round(DifferenceBtw/onday);
+	return accurateDiff;
 };
 
-// Do not change the code below.
+// Do not change the code bel
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
